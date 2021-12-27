ea0a0d5c4751b71d4514397efb479034e4083006
talang <513161384@qq.com>
2021-12-27 20:20:38 +0800

    Update index.tsx

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -25,13 +25,11 @@ function Counter() {
       <p>{count}</p>
       <button
         onClick={() => setCount(count - 1)}
-        disabled={!IS_BROWSER}
       >
         -1
       </button>
       <button
         onClick={() => setCount(count + 1)}
-        disabled={!IS_BROWSER}
       >
         +1
       </button>
